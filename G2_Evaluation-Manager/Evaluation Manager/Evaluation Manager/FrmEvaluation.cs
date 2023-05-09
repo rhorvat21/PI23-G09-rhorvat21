5ed2560945ea83698c6424f7b7c48859bf6ac3f8
rhorvat21 <120255981+rhorvat21@users.noreply.github.com>
2023-05-09 12:33:26 +0200

    Add files via upload

---
+++ G2_Evaluation-Manager/Evaluation Manager/Evaluation Manager/FrmEvaluation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Evaluation_Manager
+{
+    public partial class FrmEvaluation : Form
+    {
+        public FrmEvaluation(Models.Student student)
+        {
+            InitializeComponent();
+        }
+
+        private void FrmEvaluation_Load(object sender, EventArgs e) {
+            var actvities = ActivityRepository.GetActivities();
+            cboActivity
+        }
+    }
+}
